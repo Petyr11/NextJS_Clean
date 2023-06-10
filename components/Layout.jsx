777c68b7f744627e70f86d5f42a2056016b65cef
JP Araujo <joaopedroduarte11@gmail.com>
2023-06-10 02:55:26 -0300

    1

---
+++ components/Layout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+import Head from "next/head";
+
+const layout = ({ children }) => {
+  
+  return (
+    <>
+      <Head>
+        <title> Por favor Funciona</title>
+      </Head>
+      <Navbar></Navbar>
+      <main>{children}</main>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default layout;
